6889b7abff15a552689baa3c5613b45c10742ff3
oussamaibra <oussamabr58@gmail.com>
2024-07-15 12:06:07 +0100

    stripe V1

--- app.ts
+++ app.ts
@@ -16,6 +16,16 @@ import auth from "./routers/auth";
 import orders from "./routers/orders";
 import uploadMiddleware from "./middlewares/multerMid";
 import fs from "fs";
+import Stripe from "stripe";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const Key: any = process.env.STRIPE_SECRET_KEY;
+
+const stripe = new Stripe(Key, {
+  apiVersion: "2024-06-20",
+});
 
 const pathimg = "./uploads";
 
@@ -69,6 +79,36 @@ app.post("/api/upload", uploadMiddleware, async (req, res) => {
 
 app.get("/images/:filename", function (request, response) {
   response.sendFile(request.params.filename, { root: pathimg });
+});
+
+//****************** stripe Payment   ********************** */
+app.get("/api/v1/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+app.post("/api/v1/create-payment-intent", async (req, res) => {
+  try {
+    const totalPrice: string | undefined = req.body.totalPrice;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "EUR",
+      amount: Number(totalPrice) ?? 1,
+      automatic_payment_methods: { enabled: true },
+    });
+
+    // Send publishable key and PaymentIntent details to client
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e: any) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
 });
 
 // 404 error if route not found
